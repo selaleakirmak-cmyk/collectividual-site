b0182bd518f86d58093ca46332e3c75735d2b913
selaleakirmak-cmyk <selale.akirmak@gmail.com>
2026-04-18 02:43:44 +0300

    Add files via upload

---
+++ freuds_tarot_canva_asset_manifest.js
@@ -0,0 +1,644 @@
+window.FREUDS_TAROT_CANVA_ASSET_MANIFEST = {
+  "product": "FREUD'S TAROT",
+  "room": "Freud's Room",
+  "source_design": {
+    "design_id": "DAHHGOE6P-4",
+    "design_title": "Freudyen_Tarot_Kartlar_Canva_Editable.pptx",
+    "design_url": "https://www.canva.com/design/DAHHGOE6P-4/edit",
+    "page_count": 78
+  },
+  "mapping_rule": "Canva page order equals final deck order confirmed by user.",
+  "deck_order": {
+    "1-22": "Major Arcana",
+    "23-36": "Desire",
+    "37-50": "Fantasy",
+    "51-64": "Anxiety",
+    "65-78": "Reality"
+  },
+  "cards": [
+    {
+      "page_index": 1,
+      "card_id": "major-00",
+      "card_title": "Symptom",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 2,
+      "card_id": "major-01",
+      "card_title": "Dream-Maker",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 3,
+      "card_id": "major-02",
+      "card_title": "Repression",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 4,
+      "card_id": "major-03",
+      "card_title": "Mother",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 5,
+      "card_id": "major-04",
+      "card_title": "Father",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 6,
+      "card_id": "major-05",
+      "card_title": "Analyst",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 7,
+      "card_id": "major-06",
+      "card_title": "Ambivalence",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 8,
+      "card_id": "major-07",
+      "card_title": "Ego",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 9,
+      "card_id": "major-08",
+      "card_title": "Sublimation",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 10,
+      "card_id": "major-09",
+      "card_title": "Regression",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 11,
+      "card_id": "major-10",
+      "card_title": "Repetition",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 12,
+      "card_id": "major-11",
+      "card_title": "Superego",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 13,
+      "card_id": "major-12",
+      "card_title": "Inhibition",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 14,
+      "card_id": "major-13",
+      "card_title": "Mourning",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 15,
+      "card_id": "major-14",
+      "card_title": "Compromise Formation",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 16,
+      "card_id": "major-15",
+      "card_title": "Drive",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 17,
+      "card_id": "major-16",
+      "card_title": "Collapse",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 18,
+      "card_id": "major-17",
+      "card_title": "Idealization",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 19,
+      "card_id": "major-18",
+      "card_title": "The Unconscious",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 20,
+      "card_id": "major-19",
+      "card_title": "Insight",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 21,
+      "card_id": "major-20",
+      "card_title": "Interpretation",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 22,
+      "card_id": "major-21",
+      "card_title": "Working-Through",
+      "arcana": "major",
+      "suit": null,
+      "rank": null
+    },
+    {
+      "page_index": 23,
+      "card_id": "desire-ace",
+      "card_title": "Ace of Desire",
+      "arcana": "minor",
+      "suit": "desire",
+      "rank": "ace"
+    },
+    {
+      "page_index": 24,
+      "card_id": "desire-02",
+      "card_title": "2 of Desire",
+      "arcana": "minor",
+      "suit": "desire",
+      "rank": "02"
+    },
+    {
+      "page_index": 25,
+      "card_id": "desire-03",
+      "card_title": "3 of Desire",
+      "arcana": "minor",
+      "suit": "desire",
+      "rank": "03"
+    },
+    {
+      "page_index": 26,
+      "card_id": "desire-04",
+      "card_title": "4 of Desire",
+      "arcana": "minor",
+      "suit": "desire",
+      "rank": "04"
+    },
+    {
+      "page_index": 27,
+      "card_id": "desire-05",
+      "card_title": "5 of Desire",
+      "arcana": "minor",
+      "suit": "desire",
+      "rank": "05"
+    },
+    {
+      "page_index": 28,
+      "card_id": "desire-06",
+      "card_title": "6 of Desire",
+      "arcana": "minor",
+      "suit": "desire",
+      "rank": "06"
+    },
+    {
+      "page_index": 29,
+      "card_id": "desire-07",
+      "card_title": "7 of Desire",
+      "arcana": "minor",
+      "suit": "desire",
+      "rank": "07"
+    },
+    {
+      "page_index": 30,
+      "card_id": "desire-08",
+      "card_title": "8 of Desire",
+      "arcana": "minor",
+      "suit": "desire",
+      "rank": "08"
+    },
+    {
+      "page_index": 31,
+      "card_id": "desire-09",
+      "card_title": "9 of Desire",
+      "arcana": "minor",
+      "suit": "desire",
+      "rank": "09"
+    },
+    {
+      "page_index": 32,
+      "card_id": "desire-10",
+      "card_title": "10 of Desire",
+      "arcana": "minor",
+      "suit": "desire",
+      "rank": "10"
+    },
+    {
+      "page_index": 33,
+      "card_id": "desire-page",
+      "card_title": "Page of Desire",
+      "arcana": "minor",
+      "suit": "desire",
+      "rank": "page"
+    },
+    {
+      "page_index": 34,
+      "card_id": "desire-knight",
+      "card_title": "Knight of Desire",
+      "arcana": "minor",
+      "suit": "desire",
+      "rank": "knight"
+    },
+    {
+      "page_index": 35,
+      "card_id": "desire-queen",
+      "card_title": "Queen of Desire",
+      "arcana": "minor",
+      "suit": "desire",
+      "rank": "queen"
+    },
+    {
+      "page_index": 36,
+      "card_id": "desire-king",
+      "card_title": "King of Desire",
+      "arcana": "minor",
+      "suit": "desire",
+      "rank": "king"
+    },
+    {
+      "page_index": 37,
+      "card_id": "fantasy-ace",
+      "card_title": "Ace of Fantasy",
+      "arcana": "minor",
+      "suit": "fantasy",
+      "rank": "ace"
+    },
+    {
+      "page_index": 38,
+      "card_id": "fantasy-02",
+      "card_title": "2 of Fantasy",
+      "arcana": "minor",
+      "suit": "fantasy",
+      "rank": "02"
+    },
+    {
+      "page_index": 39,
+      "card_id": "fantasy-03",
+      "card_title": "3 of Fantasy",
+      "arcana": "minor",
+      "suit": "fantasy",
+      "rank": "03"
+    },
+    {
+      "page_index": 40,
+      "card_id": "fantasy-04",
+      "card_title": "4 of Fantasy",
+      "arcana": "minor",
+      "suit": "fantasy",
+      "rank": "04"
+    },
+    {
+      "page_index": 41,
+      "card_id": "fantasy-05",
+      "card_title": "5 of Fantasy",
+      "arcana": "minor",
+      "suit": "fantasy",
+      "rank": "05"
+    },
+    {
+      "page_index": 42,
+      "card_id": "fantasy-06",
+      "card_title": "6 of Fantasy",
+      "arcana": "minor",
+      "suit": "fantasy",
+      "rank": "06"
+    },
+    {
+      "page_index": 43,
+      "card_id": "fantasy-07",
+      "card_title": "7 of Fantasy",
+      "arcana": "minor",
+      "suit": "fantasy",
+      "rank": "07"
+    },
+    {
+      "page_index": 44,
+      "card_id": "fantasy-08",
+      "card_title": "8 of Fantasy",
+      "arcana": "minor",
+      "suit": "fantasy",
+      "rank": "08"
+    },
+    {
+      "page_index": 45,
+      "card_id": "fantasy-09",
+      "card_title": "9 of Fantasy",
+      "arcana": "minor",
+      "suit": "fantasy",
+      "rank": "09"
+    },
+    {
+      "page_index": 46,
+      "card_id": "fantasy-10",
+      "card_title": "10 of Fantasy",
+      "arcana": "minor",
+      "suit": "fantasy",
+      "rank": "10"
+    },
+    {
+      "page_index": 47,
+      "card_id": "fantasy-page",
+      "card_title": "Page of Fantasy",
+      "arcana": "minor",
+      "suit": "fantasy",
+      "rank": "page"
+    },
+    {
+      "page_index": 48,
+      "card_id": "fantasy-knight",
+      "card_title": "Knight of Fantasy",
+      "arcana": "minor",
+      "suit": "fantasy",
+      "rank": "knight"
+    },
+    {
+      "page_index": 49,
+      "card_id": "fantasy-queen",
+      "card_title": "Queen of Fantasy",
+      "arcana": "minor",
+      "suit": "fantasy",
+      "rank": "queen"
+    },
+    {
+      "page_index": 50,
+      "card_id": "fantasy-king",
+      "card_title": "King of Fantasy",
+      "arcana": "minor",
+      "suit": "fantasy",
+      "rank": "king"
+    },
+    {
+      "page_index": 51,
+      "card_id": "anxiety-ace",
+      "card_title": "Ace of Anxiety",
+      "arcana": "minor",
+      "suit": "anxiety",
+      "rank": "ace"
+    },
+    {
+      "page_index": 52,
+      "card_id": "anxiety-02",
+      "card_title": "2 of Anxiety",
+      "arcana": "minor",
+      "suit": "anxiety",
+      "rank": "02"
+    },
+    {
+      "page_index": 53,
+      "card_id": "anxiety-03",
+      "card_title": "3 of Anxiety",
+      "arcana": "minor",
+      "suit": "anxiety",
+      "rank": "03"
+    },
+    {
+      "page_index": 54,
+      "card_id": "anxiety-04",
+      "card_title": "4 of Anxiety",
+      "arcana": "minor",
+      "suit": "anxiety",
+      "rank": "04"
+    },
+    {
+      "page_index": 55,
+      "card_id": "anxiety-05",
+      "card_title": "5 of Anxiety",
+      "arcana": "minor",
+      "suit": "anxiety",
+      "rank": "05"
+    },
+    {
+      "page_index": 56,
+      "card_id": "anxiety-06",
+      "card_title": "6 of Anxiety",
+      "arcana": "minor",
+      "suit": "anxiety",
+      "rank": "06"
+    },
+    {
+      "page_index": 57,
+      "card_id": "anxiety-07",
+      "card_title": "7 of Anxiety",
+      "arcana": "minor",
+      "suit": "anxiety",
+      "rank": "07"
+    },
+    {
+      "page_index": 58,
+      "card_id": "anxiety-08",
+      "card_title": "8 of Anxiety",
+      "arcana": "minor",
+      "suit": "anxiety",
+      "rank": "08"
+    },
+    {
+      "page_index": 59,
+      "card_id": "anxiety-09",
+      "card_title": "9 of Anxiety",
+      "arcana": "minor",
+      "suit": "anxiety",
+      "rank": "09"
+    },
+    {
+      "page_index": 60,
+      "card_id": "anxiety-10",
+      "card_title": "10 of Anxiety",
+      "arcana": "minor",
+      "suit": "anxiety",
+      "rank": "10"
+    },
+    {
+      "page_index": 61,
+      "card_id": "anxiety-page",
+      "card_title": "Page of Anxiety",
+      "arcana": "minor",
+      "suit": "anxiety",
+      "rank": "page"
+    },
+    {
+      "page_index": 62,
+      "card_id": "anxiety-knight",
+      "card_title": "Knight of Anxiety",
+      "arcana": "minor",
+      "suit": "anxiety",
+      "rank": "knight"
+    },
+    {
+      "page_index": 63,
+      "card_id": "anxiety-queen",
+      "card_title": "Queen of Anxiety",
+      "arcana": "minor",
+      "suit": "anxiety",
+      "rank": "queen"
+    },
+    {
+      "page_index": 64,
+      "card_id": "anxiety-king",
+      "card_title": "King of Anxiety",
+      "arcana": "minor",
+      "suit": "anxiety",
+      "rank": "king"
+    },
+    {
+      "page_index": 65,
+      "card_id": "reality-ace",
+      "card_title": "Ace of Reality",
+      "arcana": "minor",
+      "suit": "reality",
+      "rank": "ace"
+    },
+    {
+      "page_index": 66,
+      "card_id": "reality-02",
+      "card_title": "2 of Reality",
+      "arcana": "minor",
+      "suit": "reality",
+      "rank": "02"
+    },
+    {
+      "page_index": 67,
+      "card_id": "reality-03",
+      "card_title": "3 of Reality",
+      "arcana": "minor",
+      "suit": "reality",
+      "rank": "03"
+    },
+    {
+      "page_index": 68,
+      "card_id": "reality-04",
+      "card_title": "4 of Reality",
+      "arcana": "minor",
+      "suit": "reality",
+      "rank": "04"
+    },
+    {
+      "page_index": 69,
+      "card_id": "reality-05",
+      "card_title": "5 of Reality",
+      "arcana": "minor",
+      "suit": "reality",
+      "rank": "05"
+    },
+    {
+      "page_index": 70,
+      "card_id": "reality-06",
+      "card_title": "6 of Reality",
+      "arcana": "minor",
+      "suit": "reality",
+      "rank": "06"
+    },
+    {
+      "page_index": 71,
+      "card_id": "reality-07",
+      "card_title": "7 of Reality",
+      "arcana": "minor",
+      "suit": "reality",
+      "rank": "07"
+    },
+    {
+      "page_index": 72,
+      "card_id": "reality-08",
+      "card_title": "8 of Reality",
+      "arcana": "minor",
+      "suit": "reality",
+      "rank": "08"
+    },
+    {
+      "page_index": 73,
+      "card_id": "reality-09",
+      "card_title": "9 of Reality",
+      "arcana": "minor",
+      "suit": "reality",
+      "rank": "09"
+    },
+    {
+      "page_index": 74,
+      "card_id": "reality-10",
+      "card_title": "10 of Reality",
+      "arcana": "minor",
+      "suit": "reality",
+      "rank": "10"
+    },
+    {
+      "page_index": 75,
+      "card_id": "reality-page",
+      "card_title": "Page of Reality",
+      "arcana": "minor",
+      "suit": "reality",
+      "rank": "page"
+    },
+    {
+      "page_index": 76,
+      "card_id": "reality-knight",
+      "card_title": "Knight of Reality",
+      "arcana": "minor",
+      "suit": "reality",
+      "rank": "knight"
+    },
+    {
+      "page_index": 77,
+      "card_id": "reality-queen",
+      "card_title": "Queen of Reality",
+      "arcana": "minor",
+      "suit": "reality",
+      "rank": "queen"
+    },
+    {
+      "page_index": 78,
+      "card_id": "reality-king",
+      "card_title": "King of Reality",
+      "arcana": "minor",
+      "suit": "reality",
+      "rank": "king"
+    }
+  ]
+};
